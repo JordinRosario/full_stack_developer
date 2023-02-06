47a786895b61d43a37196046a072850ffec6d582
JordinRosario <jordinjose2003@gmail.com>
2023-02-05 21:26:05 -0500

    Bootrap

---
+++ html_basic/prueba.py
@@ -0,0 +1,12 @@
+precio_postre = 350.00 
+botella_de_agua = 20.00
+quipes = 0.00
+
+
+total_de_postre = precio_postre * 4
+total_de_botella_de_agua = botella_de_agua *4
+total_quipe = quipes *4
+
+total_cuenta = total_de_postre + total_de_botella_de_agua + total_quipe
+
+print(f'El total de la cuenta es :{total_cuenta} pesos')
